827001c3c8261ca90822098cc7d37267b6e117f6
RobinHYuan <circleyhg001@gmail.com>
2022-03-15 02:36:19 -0700

    encoder distance read done

---
+++ MCU and PCB/Arduino Code/Test_Blink/Test_Blink.ino
@@ -0,0 +1,26 @@
+#define phaseA 22
+#define phaseB 24
+
+int LastStateA  = digitalRead(phaseA);
+int LastStateB  = digitalRead(phaseB);
+int counter = 0;
+
+void setup() {
+  pinMode (phaseA, INPUT);
+  pinMode (phaseB, INPUT);
+  Serial.begin (9600);
+}
+
+void loop() {
+  
+  int CurrentStateA  = digitalRead(phaseA);
+  int CurrentStateB  = digitalRead(phaseB);
+
+ if (LastStateA != CurrentStateA) 
+    if (CurrentStateB == LastStateA) counter--;
+      else counter++;
+    
+  Serial.print (counter);
+  Serial.print ("\n");
+  LastStateA = CurrentStateA;
+}
